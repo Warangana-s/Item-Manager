3eadf59f10cb97f60c0dd15efd050ff97c39d538
Warangana-s <sathminim8@gmail.com>
2026-04-28 11:56:51 +0530

    Updated

---
+++ WMT_LabTest_Project/backend/server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import itemRoutes from "./routes/itemRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+
+app.use(
+  cors({
+    origin: "*", // you can replace with your Vercel URL later
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+
+app.get("/", (req, res) => {
+  res.json({ message: "Item Manager API is running..." });
+});
+
+
+app.use("/api/items", itemRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection error:", error.message);
+    process.exit(1);
+  });
